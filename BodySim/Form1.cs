243e56f23636b56b5904a387e72270907a5689ab
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2024-08-10 00:22:57 +0200

    UI mockup

--- BodySim/Form1.cs
+++ BodySim/Form1.cs
@@ -16,5 +16,35 @@ namespace BodySim
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
